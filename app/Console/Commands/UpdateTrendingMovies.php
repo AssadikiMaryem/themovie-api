4f5fe184fae8f43014e2c10524f07e0cbcf10290
Maryem <maryem.assadiki@gmail.com>
2023-12-03 23:49:29 +0100

    Use Validator / Comment code

--- app/Console/Commands/UpdateTrendingMovies.php
+++ app/Console/Commands/UpdateTrendingMovies.php
@@ -3,9 +3,11 @@ declare(strict_types=1);
 
 namespace App\Console\Commands;
 
+use App\Http\Requests\UpdateTrendingMoviesRequest;
 use App\Services\MovieService;
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\Log;
+use Illuminate\Validation\ValidationException;
 
 class UpdateTrendingMovies extends Command
 {
@@ -38,6 +40,10 @@ class UpdateTrendingMovies extends Command
         //Update trending movies by day
         try {
             $this->movieService->updateTrendingMovies('day');
+            $this->info('The command was successful! (The trending movies by day are updated)');
+        } catch (ValidationException $e) {
+            $this->error($e->getMessage());
+            Log::error($e->getMessage());
         } catch (\Exception $e) {
             Log::error($e->getMessage());
         }
@@ -47,6 +53,10 @@ class UpdateTrendingMovies extends Command
         //Update trending movies by week
         try {
             $this->movieService->updateTrendingMovies('week');
+            $this->info('The command was successful! (The trending movies by week are updated)');
+        } catch (ValidationException $e) {
+            $this->error($e->getMessage());
+            Log::error($e->getMessage());
         } catch (\Exception $e) {
             Log::error($e->getMessage());
         }
